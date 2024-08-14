0d3238ac1714368e6249cda41870389d83c66a31
rivertogg <167750764+stoogg@users.noreply.github.com>
2024-08-14 13:48:39 +0900

    Add MainMenuBar
    "Button" 클릭 > 해당 버튼 로그 출력
    "NewGame" 클릭 > 씬 전환

---
+++ Assets/Script/ButtonScr.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class ButtonScr : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void OnClickNewGame()
+    {
+        Debug.Log("NewGame");
+        SceneManager.LoadScene("_MinsungTest");
+    }
+    public void OnClickRoadGame()
+    {
+        Debug.Log("RoadGame");
+    }
+    public void OnClickOption()
+    {
+        Debug.Log("Option");
+    }
+    public void OnClickEnd()
+    {
+        Debug.Log("End");
+    }
+
+}
