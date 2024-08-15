01c50e8e0f3d44d0c3d550cdcd00955c393891c5
blueskys95 <jjh951110@naver.com>
2024-08-15 19:17:13 +0900

    Player에게 카메라 조정
    Player에 메인카메라가 고정된다.

--- Assets/Script/Bullet.cs
+++ Assets/Script/Bullet.cs
@@ -4,8 +4,22 @@ using UnityEngine;
 
 public class Bullet : MonoBehaviour
 {
+    public float damage = 20.0f;
+
+    public float force = 1.0f;
+
+    private Rigidbody Rigid;
+
+    private void Start()
+    {
+        Rigid = GetComponent<Rigidbody>();
+
+        Rigid.AddForce(transform.forward * force);
+    }
+
     void Update()
     {
-        transform.Translate(new Vector3(0.05f,0,0));
+        Rigid.AddForce(transform.forward * force);
+        transform.Translate(new Vector3(0.05f, 0, 0));
     }
 }
