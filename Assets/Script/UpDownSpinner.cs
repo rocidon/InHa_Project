a54de054a475945a5e77a26a3d9089652440edf3
GGaeRuk <sean2010@naver.com>
2024-08-17 17:43:13 +0900

    SceneTest
    씬 작업
    장애물 이동 관련 작업중

---
+++ Assets/Script/UpDownSpinner.cs
@@ -0,0 +1,68 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEditor.Timeline;
+using UnityEngine;
+
+public class UpDownSpinner : MonoBehaviour
+{
+    // À§¾Æ·¡
+    float initPositionY;
+    float initPositionX;
+    public float distance;
+    public float turningPoint;
+
+    public bool turnSwitch;
+    public float moveSpeed;
+
+    public float rotateSpeed;
+    
+    void Awake()
+    {
+        if (gameObject.name == "BladeSpinner");
+        {
+            initPositionY = transform.position.y;
+            turningPoint = initPositionY - distance;
+        }
+    }
+
+    void upDown()
+    {
+        float currentPositionY = transform.position.y;
+        if (currentPositionY >= initPositionY)
+        {
+            turnSwitch = false;
+        }
+        else if (currentPositionY <= initPositionY)
+        {
+            turnSwitch = true;
+        }
+        if (turnSwitch)
+        {
+            transform.position = transform.position + new Vector3(0, 1, 0)
+                * moveSpeed * Time.deltaTime;
+        }
+        else
+        {
+            transform.position = transform.position + new Vector3(0, -1, 0)
+                * moveSpeed * Time.deltaTime;
+        }
+    }
+
+    void rotate()
+    {
+        transform.Rotate(Vector3.right * rotateSpeed * Time.deltaTime);
+    }
+
+    void Update()
+    {
+        if (gameObject.name == "BladeSpinner")
+        {
+            upDown();
+        }
+        if (gameObject.name == "RT_Floor")
+        {
+            rotate();
+        }
+    }
+
+}
