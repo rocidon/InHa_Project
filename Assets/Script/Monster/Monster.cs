da1bb2788faa683a382fcb150aa26f0146fb6d44
rocidon <43723501+rocidon@users.noreply.github.com>
2024-08-17 17:36:10 +0900

    80% complete
    Enemy

--- Assets/Script/Monster/Monster.cs
+++ Assets/Script/Monster/Monster.cs
@@ -7,8 +7,7 @@ public class Monster : MonoBehaviour
     protected float _Health;
     protected float _Atk;
     protected float _Def;
-    protected FSM _fsm;
-    
+    protected FSM _fsm;    
     private enum MonsterState
     {
         Idle,
@@ -23,19 +22,19 @@ public class Monster : MonoBehaviour
     {
         _state = MonsterState.Idle;
     }
-    private void Update()
-    {
-        switch (_state) { 
-            case MonsterState.Idle:
-                break;
-            case MonsterState.Move:
-                break;
-            case MonsterState.Attack:
-                break;
-            case MonsterState.Death:
-                break;
-        }
-    }
+    //private void Update()
+    //{
+    //    switch (_state) { 
+    //        case MonsterState.Idle:
+    //            break;
+    //        case MonsterState.Move:
+    //            break;
+    //        case MonsterState.Attack:
+    //            break;
+    //        case MonsterState.Death:
+    //            break;
+    //    }
+    //}
     protected bool IsDeath()
     {
         if(_Health <= 0.0f)
--- Assets/Script/Monster/NormalMoster.cs
+++ Assets/Script/Monster/NormalMoster.cs
@@ -2,8 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.UIElements;
 //https://danpung2.tistory.com/58
-public class NormalMoster : Monster
+public class NormalMonster : Monster
 {
     RaycastHit hit;
     enum State
@@ -11,47 +13,52 @@ public class NormalMoster : Monster
         Idle,
         Move,
         Attack,
+        See,
         Death
     }
     State _currentState;
     public float speed;
+    float oSpeed;
+    public float AtkRange;
+    Field_of_View fov;
     //FSM _fsm;
     void Start()
     {
+        fov = GetComponent<Field_of_View>();
+        //Debug.Log(fov.a);
         _Health = 200.0f;
         _Atk = 10.0f;
         _Def = 1.0f;
         _currentState = State.Idle;
         _fsm = new FSM(new IdleState(this));
         speed = speed >= 1.0f ? speed : 5.0f;
+        oSpeed = speed;
+        AtkRange = AtkRange >= 1.0f ? AtkRange : 3.0f;
     }
 
     // Update is called once per frame
     void Update()
     {
-        //변경 필요
+        Debug.Log(fov.AtkPlayer);
+        Debug.Log(_currentState); 
         switch (_currentState)
         {
+            
             case State.Idle:
-                if (true)//see player
+                if (fov.FindPlayer == false)
                 {
-                    if(_Health > 0)//canatkplayer
-                    {
-                        ChangeState(State.Attack);
-                    }
-                    else
-                    {
-                        ChangeState(State.Move);
-                    }
+                    ChangeState(State.Move);                   
+                }
+                else
+                {
+                    ChangeState(State.See);
                 }
                 break;
             case State.Move:
-                if(_Health> 0)//see player
+                speed = oSpeed;
+                if (fov.FindPlayer)
                 {
-                    if(_Health >0)//atk player
-                    {
-                        ChangeState(State.Attack);
-                    }
+                    ChangeState(State.See);
                 }
                 else
                 {
@@ -59,16 +66,24 @@ public class NormalMoster : Monster
                 }
                 break;
             case State.Attack:
-                if(_Health > 0)//see player
+
+                if (fov.AtkPlayer== false)
                 {
-                    if(_Health>0)//!atk player
+                    ChangeState(State.See);
+                }
+                break;
+            case State.See:
+                speed = 0.5f;
+                if (fov.FindPlayer)
+                {
+                    if (fov.AtkPlayer)
                     {
-                        ChangeState(State.Move);
-                    }
+                        ChangeState(State.Attack);
+                    }               
                 }
                 else
                 {
-                    ChangeState(State.Idle);
+                    ChangeState (State.Idle);
                 }
                 break;
             case State.Death:
@@ -96,6 +111,9 @@ public class NormalMoster : Monster
             case State.Attack:
                 _fsm.ChangeState(new AttackState(this));
                 break;
+            case State.See:
+                _fsm.ChangeState(new SeeState(this));
+                break;
         }
     }
 
@@ -117,14 +135,20 @@ public class NormalMoster : Monster
             transform.forward = BackVec;
         }
     }
+    bool DetectedPLayer()
+    {
+        Vector3 CurrentPos = transform.position;
+
+        return true;
+    }
     
 }
 
 //각 행동 State에 대해 정의해야함
 public class IdleState : BaseState
 {
-    private NormalMoster _normalMob;
-    public IdleState(NormalMoster monster) : base(monster) {
+    private NormalMonster _normalMob;
+    public IdleState(NormalMonster monster) : base(monster) {
         _normalMob = monster;
     }
 
@@ -144,8 +168,8 @@ public class IdleState : BaseState
 }
 public class MoveState : BaseState
 {
-    private NormalMoster _normalMob;
-    public MoveState(NormalMoster monster) : base(monster) {
+    private NormalMonster _normalMob;
+    public MoveState(NormalMonster monster) : base(monster) {
         _normalMob = monster;
     }
     
@@ -167,8 +191,8 @@ public class MoveState : BaseState
 }
 public class AttackState : BaseState
 {
-    private NormalMoster _normalMob;
-    public AttackState(NormalMoster monster) : base(monster)
+    private NormalMonster _normalMob;
+    public AttackState(NormalMonster monster) : base(monster)
     {
         _normalMob = monster;
     }
@@ -179,6 +203,30 @@ public class AttackState : BaseState
     }
     public override void onStateUpdate()
     {
+        //Debug.Log("Attack!");
+        //throw new System.NotImplementedException();
+    }
+
+    public override void onStateExit()
+    {
+        //throw new System.NotImplementedException();
+    }
+}
+
+public class SeeState : BaseState
+{
+    private NormalMonster _normalMob;
+    public SeeState(NormalMonster monster) : base(monster)
+    {
+        _normalMob = monster;
+    }
+    public override void onStateEnter()
+    {
+        //throw new System.NotImplementedException();
+    }
+    public override void onStateUpdate()
+    {
+        _normalMob.transform.Translate(Vector3.forward * _normalMob.speed * Time.deltaTime);
         //throw new System.NotImplementedException();
     }
 
