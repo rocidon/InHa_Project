57d16367523e73d0ba5bc9b0b676ab3d667f0e31
GGaeRuk <sean2010@naver.com>
2024-08-24 15:24:55 +0900

    leveldesign
    레벨 디자인 거의 완료
    장애물(오브젝트) 기능구현 완료

---
+++ Assets/Script/PsyScript/SceneChange/PotalSceneChange4.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class PotalSceneChange4 : MonoBehaviour
+{
+    // 포탈이 플레이어와 상호작용할 수 있는 범위 정의
+    public float interactionRange = 2f;
+    private bool isPlayerInRange = false;
+
+    private void Update()
+    {
+        // 플레이어가 포탈 범위에 있을 때
+        if (isPlayerInRange && Input.GetKeyDown(KeyCode.Z))
+        {
+            SceneManager.LoadScene("BossScene_Psy");
+        }
+    }
+    private void OnTriggerEnter(Collider other)
+    {
+        // 플레이어가 포탈의 범위에 들어왔을 때
+        if(other.CompareTag("Player"))
+        {
+            isPlayerInRange = true;
+        }
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        // 플레이어가 범위 밖으로 갔을 때
+        if (other.CompareTag("Player"))
+        {
+            isPlayerInRange = false;
+        }
+    }
+
+}
