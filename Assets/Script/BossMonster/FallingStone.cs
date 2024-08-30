d6c5730630b817e859266ebccf910c08a47b43d0
rocidon <43723501+rocidon@users.noreply.github.com>
2024-08-30 16:24:41 +0900

    1

--- Assets/Script/BossMonster/FallingStone.cs
+++ Assets/Script/BossMonster/FallingStone.cs
@@ -8,7 +8,7 @@ public class FallingStone : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        inst
     }
 
     // Update is called once per frame
