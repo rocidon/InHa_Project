1a9a5f1b794a0d3afc4d5c33713cf9dd0e793b5a
ParkSinYoung <sean2010@naver.com>
2024-09-07 10:14:48 +0900

    1

--- Assets/Script/PsyScript/CoinEat.cs
+++ Assets/Script/PsyScript/CoinEat.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class CoinEat : MonoBehaviour
 {
-    
+    public CurrencyData currencyData;
     // ÄÚÀÎ ¸Ô±â
     private void OnTriggerEnter(Collider collision)
     {
         if(collision.gameObject.tag == "Player")
         {
             //collision.gameObject.GetComponent<PlayerMove>().coin++;
+            currencyData.curGoldAmount += 100;
             Destroy(this.gameObject);
         }
     }
