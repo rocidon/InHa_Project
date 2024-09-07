647d012167649b366489459ccac9fc68049e928c
ParkSinYoung <sean2010@naver.com>
2024-09-07 14:13:10 +0900

    소

--- Assets/Script/PsyScript/FollowCamera.cs
+++ Assets/Script/PsyScript/FollowCamera.cs
@@ -3,7 +3,7 @@ using System.Collections.Generic;
 using UnityEngine;
 
 public class FollowCamera : MonoBehaviour
-{
+{   
     [SerializeField] GameObject player;
     [SerializeField] float x;
     [SerializeField] float y;
