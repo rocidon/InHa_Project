9ba52d13838068458afe56ad4fd4f1f15520bdfd
Jaehoon <jjh951110@naver.com>
2024-09-20 18:30:11 +0900

    보스전 카메라 BossCamera 스크립트 생성 후 보스씬에 적용

---
+++ Assets/BossCamera.cs
@@ -0,0 +1,25 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Security.Cryptography;
+using UnityEngine;
+using UnityEngine.UIElements;
+using static UnityEditor.Experimental.GraphView.GraphView;
+
+public class BossCamera : MonoBehaviour
+{
+    [SerializeField] Transform player;
+    [SerializeField] float smoothing = 0.2f;
+    [SerializeField] Vector2 minCameraBoundary;
+    [SerializeField] Vector2 maxCameraBoundary;
+    // Update is called once per frame
+    private void FixedUpdate()
+    {
+        Vector3 targetPos = new Vector3(player.position.x, player.position.y, transform.position.z);
+
+        targetPos.x = Mathf.Clamp(targetPos.x, minCameraBoundary.x, maxCameraBoundary.x);
+        targetPos.y = Mathf.Clamp(targetPos.y, minCameraBoundary.y, maxCameraBoundary.y);
+
+        transform.position = Vector3.Lerp(transform.position, targetPos, smoothing);
+    }
+
+}
